22c05cd9e1343734776ebf9a54d56321bcddbd96
emadni <emadkarimi96@gmail.com>
2016-01-10 18:25:13 +1000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {telelordteam},
+    sudo_users = telelordteam{telelordteam},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
